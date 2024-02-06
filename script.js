e5bf8710bf05c17283175842eae078e7b2d11ea5
talhajutt-01 <talhaj928@gmail.com>
2024-02-06 20:04:47 +0500

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-gsap.registerPlugin(ScrollTrigger);
-
 // Pinning the right div to the background in the center of the screen
 ScrollTrigger.create({
     trigger: ".gallery",
@@ -13,13 +11,30 @@ ScrollTrigger.create({
 gsap.utils.toArray('.details').forEach((details, index) => {
     const tl = gsap.timeline({
         scrollTrigger: {
-			scrub: 1,
+            scrub: 1,
             trigger: details,
             start: "top 60%", // Adjust this value as needed
             end: "bottom 30%", // Adjust this value as needed
             toggleActions: "play none none none",
+            onEnter: () => {
+                // Hide all photo divs initially
+                document.querySelectorAll('.right > div').forEach(div => {
+                    div.style.display = 'none';
+                });
+                // Show the corresponding photo div based on the index
+                document.querySelector(`.photos${index + 1}`).style.display = 'block';
+            },
+            onLeaveBack: () => {
+                // Show the previous photo div when scrolling upwards
+                if (index > 0) {
+                    document.querySelectorAll('.right > div').forEach(div => {
+                        div.style.display = 'none';
+                    });
+                    document.querySelector(`.photos${index}`).style.display = 'block';
+                }
+            }
         }
     });
-    
-    tl.fromTo(details, { opacity: 0, y: 50, }, { opacity: 1, y: 0, duration: 0.7 });
+
+    tl.fromTo(details, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 0.7 });
 });
